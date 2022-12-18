ac7b2bab068d5479e684a14992e4eb4e9208b7f7
franck <carnet.franck.paul@gmail.com>
2022-12-18 11:04:59 +0100

    Release 2.1

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Carnaval',
     'Identify comments',
     'Osku and contributors',
-    '2.0',
+    '2.1',
     [
         'requires'    => [['core', '2.24']],
         'permissions' => dcCore::app()->auth->makePermissions([
