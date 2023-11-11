160411b3c42733b1de1ce03c90458cbb1d5361a1
franck <carnet.franck.paul@gmail.com>
2023-11-11 13:38:32 +0100

    New release 5.3

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Carnaval',
     'Identify comments',
     'Osku and contributors',
-    '5.2.1',
+    '5.3',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => '',
